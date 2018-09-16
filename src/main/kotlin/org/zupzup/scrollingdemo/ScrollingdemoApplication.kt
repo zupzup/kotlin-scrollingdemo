fc6f18ab3e7c0a93c93c5ffb47f08a96d0536619
Mario Zupan <mzupanmz@gmail.com>
2018-09-16 10:20:39 +0200

    init

---
+++ src/main/kotlin/org/zupzup/scrollingdemo/ScrollingdemoApplication.kt
@@ -0,0 +1,11 @@
+package org.zupzup.scrollingdemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ScrollingdemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<ScrollingdemoApplication>(*args)
+}
